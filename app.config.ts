51f24b07e2a6db523d3f1d23c1ebde8971036503
Petr Faitl <pfaitl@gmail.com>
2023-07-15 17:13:39 +1200

    Adds Block hero; settings tweaks

--- app.config.ts
+++ app.config.ts
@@ -1,15 +1,15 @@
 export default defineAppConfig({
   alpine: {
-    title: 'Alpine',
+    title: 'Alpine Playground',
     description: 'The minimalist blog theme',
     image: {
       src: '/social-card-preview.png',
       alt: 'An image showcasing my project.',
-      width: 400,
+      width: 800,
       height: 300
     },
     header: {
-      position: 'right', // possible value are : | 'left' | 'center' | 'right'
+      position: 'left', // possible value are : | 'left' | 'center' | 'right'
       logo: {
         path: '/logo.svg', // path of the logo
         pathDark: '/logo-dark.svg', // path of the logo in dark mode, leave this empty if you want to use the same logo
@@ -21,7 +21,7 @@ export default defineAppConfig({
         enabled: true, // possible value are : true | false
         repository: 'https://www.github.com/nuxt-themes/alpine' // our github repository
       },
-      navigation: true, // possible value are : true | false
+      navigation: false, // possible value are : true | false
       alignment: 'center', // possible value are : 'none' | 'left' | 'center' | 'right'
       message: 'Follow me on' // string that will be displayed in the footer (leave empty or delete to disable)
     },
--- tokens.config.ts
+++ tokens.config.ts
@@ -1,4 +1,39 @@
 import { defineTheme } from 'pinceau'
 
 export default defineTheme({
+  color: {
+    b: {},
+    bl: {},
+    blu: {},
+    ri: {},
+    rin: {},
+    rindi: {},
+    rindigo: {},
+    indigo: {},
+    indigob: {},
+    indigobl: {},
+    indigoblu: {}
+  },
+
+  alpine: {
+    body: {
+      backgroundColor: {
+        initial: '#ffffff',
+        dark: '#060318'
+      }
+    },
+
+    backdrop: {
+      backgroundColor: {
+        dark: '#18181b'
+      }
+    }
+  },
+
+  gray: {},
+  cgray: {},
+  cogray: {},
+  colgray: {},
+  cologray: {},
+  colorgray: {}
 })
